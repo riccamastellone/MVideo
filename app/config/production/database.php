a58bce3bc824b1288ab591bab332dc3f4b9a7688
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2014-07-14 14:07:52 +0200

    DB prod

---
+++ app/config/production/database.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'mvideo',
+			'username'  => 'mvideo',
+			'password'  => 'mvideo2015password',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+
+	),
+
+);
